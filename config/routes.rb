476a52afecd7527ee2f8c16f67e1fe03b0e3a4a6
Hebron George <hebrontgeorge@gmail.com>
2021-04-03 11:28:23 -0400

    Create find/add routes for quotes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post '/find-quote', to: 'quotes#find_quote'
+  post '/add-quote',  to: 'quotes#add_quote'
 end
