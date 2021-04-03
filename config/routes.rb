44cc4b2c31c536dd9921c5f244886ffe7971fae5
Hebron George <hebrontgeorge@gmail.com>
2021-04-03 11:40:19 -0400

    Add a home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root 'static_pages/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post '/find-quote', to: 'quotes#find_quote'
   post '/add-quote',  to: 'quotes#add_quote'
+
 end
