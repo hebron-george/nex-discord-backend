fd472b1512068944df899741cd8afa8325271836
Hebron George <hebrontgeorge@gmail.com>
2021-11-26 14:15:21 -0500

    Move quote look up logic from controller to model; create a Message struct to hold the result

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,3 +1,18 @@
 class Quote < ApplicationRecord
-  scope :search, -> (keyword) { where(arel_table[:message].matches("%#{keyword}%")) }
+  scope :search, -> (keyword) { where(arel_table[:message].matches("%#{keyword}%")).order("RANDOM()") }
+
+  Message = Struct.new(:author, :saved_at, :channel, :message, keyword_init: true)
+
+  def self.find_quote(search_keyword:)
+    results = Quote.search(search_keyword)
+    return nil if results.count.zero?
+
+    result = results.last
+    Message.new(
+      author:   result.quote_submitter,
+      saved_at: result.created_at,
+      channel:  result.channel,
+      message:  result.message
+    )
+  end
 end
